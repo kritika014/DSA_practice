ffa2d784b266af9868efe67fd43340a3f1e09455
kritika014 <hellokritika2006@gmail.com>
2026-08-14 16:47:55 +0530

    Time: 3 ms (54.42%) | Memory: 10.8 MB (88.62%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        map<char, int> mp;
+        for (char ch : s) {
+            mp[ch]++;
+        }
+        vector<pair<char, int>> v(mp.begin(), mp.end());
+
+        sort(v.begin(), v.end(),
+             [](auto& a, auto& b) { return a.second > b.second; });
+        string ans = "";
+        for (auto it : v) {
+            while (it.second--) {
+                ans +=(it.first);
+            }
+        }
+        return ans;
+    }
+};
