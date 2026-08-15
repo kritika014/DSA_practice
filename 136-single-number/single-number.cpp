8afb6a6a64d01272646221e82c1e671c2ff32c94
kritika014 <hellokritika2006@gmail.com>
2026-08-15 18:10:48 +0530

    Time: 8 ms (16.28%) | Memory: 23.8 MB (12.38%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int num:nums){
+            mp[num]++;
+        }
+        for(auto it:mp){
+            if(it.second==1){
+                return it.first;
+                break;
+            }
+        }
+        return 0;
+    }
+};
