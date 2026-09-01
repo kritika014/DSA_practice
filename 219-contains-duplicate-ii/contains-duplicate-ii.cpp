be291cdc371dd92960195f488a79a115d1b9dd8d
kritika014 <hellokritika2006@gmail.com>
2026-09-01 20:21:11 +0530

    Time: 115 ms (32.76%) | Memory: 142.3 MB (6.38%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n=nums.size();
+        bool ans=false;
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            int val=nums[i];
+            if(mp.find(val)!=mp.end() && i-mp[val]<=k){
+                return true;
+            }
+            mp[val]=i;
+        }
+        return false;
+    }
+};
