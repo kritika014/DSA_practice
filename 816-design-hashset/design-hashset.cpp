de2d014e6dc097f497095357d88f915ae1230312
kritika014 <hellokritika2006@gmail.com>
2026-09-13 15:18:36 +0530

    Time: 109 ms (10.68%) | Memory: 45.8 MB (97.21%) - LeetSync

---
+++ 816-design-hashset/design-hashset.cpp
@@ -0,0 +1,35 @@
+class MyHashSet {
+public:
+    MyHashSet() {
+        
+    }
+    vector<int>arr;
+    void add(int key) {
+        arr.push_back(key);
+    }
+    
+    void remove(int key) {
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==key){
+                arr.erase(arr.begin()+i);
+            }
+        }
+    }
+    
+    bool contains(int key) {
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==key){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
